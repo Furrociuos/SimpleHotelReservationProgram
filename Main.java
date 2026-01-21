b8c3a67d26bf184c3bad5496ada4861179595673
Furrociuos <marcbenj2006@gmail.com>
2026-01-21 20:31:20 +0800

    Made main menu into its own function and added more comments

--- Main.java
+++ Main.java
@@ -3,79 +3,89 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
-    File csvDirectory = new File("CSVs");
 
-    // Checks if the "CSVs" folder exists then creates it if it dosen't
-      if (csvDirectory.mkdir()) {
-        System.out.println("CSV folder does not exist. Creating...");
-      }
-    
-      listCSVs();
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        File csvDirectory = new File("CSVs");
 
-      System.out.print("Enter the name of your csv file (Without file extension): ");
-        String file = in.nextLine();
-          csvCheckandCreate(file); // Checks if the csv file exists
-
-    // Main Menu
-    boolean selection = false;
-
-      System.out.println("What would you like to do? (CTRL + C to stop the program)\n  1. Add a reservation\n  2. Remove a reservation\n  3. Modify a reservation");
-
-    while (selection == false) {
-      try {
-        System.out.print("Please enter a number: ");
-          int userChoice = in.nextInt();
-          switch (userChoice) {
-            case 1:
-      //        addReservation();
-              selection = true;
-              break;
-            case 2:
-      //        rmReservation();
-              selection = true;
-              break;
-            case 3:
-      //        modReservation();
-              selection = true;
-              break;
-            default:
-              System.out.println("Invalid! Please enter a valid number"); // Continues loop if input is invalid
+        // Checks if the "CSVs" folder exists then creates it if it dosen't
+        if (csvDirectory.mkdir()) {
+            System.out.println("CSV folder does not exist. Creating...");
         }
-    } catch (InputMismatchException e) {
-        System.out.println("Invalid! Please enter a valid number"); // Continues loop if input is invalid
-        in.nextLine();
-      }
+
+        // Lists down what files are contained in the "CSVs" folder
+        listCSVs();
+
+        System.out.print(
+            "Enter the name of your csv file (Without file extension): "
+        );
+        String file = in.nextLine();
+        csvCheckandCreate(file); // Checks if the csv file exists
+
+        // Main Menu
+        System.out.println(
+            "What would you like to do? (CTRL + C to stop the program)\n  1. Add a reservation\n  2. Remove a reservation\n  3. Modify a reservation"
+        );
+        mainMenu();
     }
-  }
-  
-  private static void listCSVs() {
-      File csvDirectory = new File("CSVs");  
-      File[] files = csvDirectory.listFiles();
 
-      System.out.println("CSVs/");
-      for (File file : files) {
-          System.out.println("  └ " + file.getName());
-      }
-  }
+    private static void listCSVs() {
+        File csvDirectory = new File("CSVs");
+        File[] files = csvDirectory.listFiles();
 
-  private static void csvCheckandCreate(String filename) {
-    try {
-      File csv = new File("CSVs", filename + ".csv");
-
-      // Informs the user that the file exists
-      if (csv.exists()) {
-        System.out.print("File found.\n");
-      }
-
-      // Creates the file if not found
-      else {
-        System.out.print("File not found. Creating...\n");
-        csv.createNewFile();
-      }
-    } catch (IOException e) {
-      System.out.println("File was not created. Do you have permission to create a file?");
+        System.out.println("CSVs/");
+        for (File file : files) {
+            System.out.println("  └ " + file.getName());
+        }
     }
-  }
+
+    private static void csvCheckandCreate(String filename) {
+        try {
+            File csv = new File("CSVs", filename + ".csv");
+
+            // Informs the user that the file exists
+            if (csv.exists()) {
+                System.out.print("File found.\n");
+            }
+            // Creates the file if not found
+            else {
+                System.out.print("File not found. Creating...\n");
+                csv.createNewFile();
+            }
+        } catch (IOException e) {
+            System.out.println(
+                "File was not created. Do you have permission to create a file?"
+            );
+        }
+    }
+
+    private static void mainMenu() {
+        boolean selection = false;
+        while (selection == false) {
+            try {
+                System.out.print("Please enter a number: ");
+                int userChoice = in.nextInt();
+                switch (userChoice) {
+                    case 1:
+                        //                        addReservation();
+                        selection = true;
+                        break;
+                    case 2:
+                        //                        rmReservation();
+                        selection = true;
+                        break;
+                    case 3:
+                        //                        modReservation();
+                        selection = true;
+                        break;
+                    default:
+                        System.out.println(
+                            "Invalid! Please enter a valid number"
+                        ); // Continues loop if input is invalid
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid! Please enter a valid number"); // Continues loop if input is invalid
+            }
+        }
+    }
 }
